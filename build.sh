ef94b01118ab849268401609f5849ff75d858623
YotioSoft <ytani0323@gmail.com>
2022-09-28 03:21:26 +0900

    complete day02b

--- build.sh
+++
@@ -1,5 +0,0 @@
-cd ../edk2
-source edksetup.sh
-build
-../osbook/devenv/run_qemu.sh Build/MikanLoaderX64/DEBUG_CLANGPDB/X64/Loader.efi
-cd ../osbook
--- devenv/vagrant-build-edk2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dd if=build-edk2.sh | vagrant ssh -c 'dd of=build-edk2.sh; chmod +x build-edk2.sh; ./build-edk2.sh'
